02f27502fede3902ec380d06754c21637ab1ffd7
Chun <fyc92330.tw@gmail.com>
2022-07-15 22:51:02 +0800

    [Add] 09_PalindromeNumber

---
+++ src/test/java/easy/_09_PalindromeNumber/Chun_09.java
@@ -0,0 +1,39 @@
+package easy._09_PalindromeNumber;
+
+import leetcode.LeetcodeMission;
+import leetcode.LeetcodeTest;
+import org.junit.jupiter.api.DisplayName;
+
+public class Chun_09 extends LeetcodeTest {
+
+    static {
+        leetcodeMission = new _09_Mission();
+    }
+
+    @DisplayName("回文數字")
+    public void main() {
+        for (LeetcodeMission.Topic topic : leetcodeMission.getTopicList()) {
+            _09_Mission.Topic09 topic09 = (_09_Mission.Topic09) topic;
+            int target = topic09.target;
+            boolean output = topic09.output;
+
+            char[] targetChars = String.valueOf(target).toCharArray();
+            int length = targetChars.length;
+            if (target < 0 || length % 2 == 0) {
+                this.printResult(String.format("%d不符合.%n", target), !output);
+                continue;
+            }
+
+            boolean flag = true;
+            for(int i = 0; i<length/2+1; i++){
+                if(targetChars[i] != targetChars[length-i-1]){
+                    flag = false;
+                    break;
+                }
+            }
+
+            this.printResult(String.format("%d%s.%n", target, flag?"符合":"不符合"), flag == output);
+        }
+    }
+
+}
---
+++ src/test/java/easy/_09_PalindromeNumber/_09_Mission.java
@@ -0,0 +1,24 @@
+package easy._09_PalindromeNumber;
+
+import leetcode.LeetcodeMission;
+
+public class _09_Mission extends LeetcodeMission {
+
+    public _09_Mission() {
+        this.setTopic(new Topic09(121, true));
+        this.setTopic(new Topic09(123, false));
+        this.setTopic(new Topic09(-121, false));
+        this.setTopic(new Topic09(10, false));
+        this.setTopic(new Topic09(1, true));
+    }
+
+    static class Topic09 extends Topic {
+        int target;
+        boolean output;
+
+        public Topic09(int target, boolean output) {
+            this.target = target;
+            this.output = output;
+        }
+    }
+}
